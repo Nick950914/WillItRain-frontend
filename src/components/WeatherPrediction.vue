f1fea0f343304bf4025437d714fa889a68f890b0
Nikola Shumarev <nick_95@mail.bg>
2019-11-25 17:54:09 +0200

    init weather prediction component

---
+++ src/components/WeatherPrediction.vue
@@ -0,0 +1,18 @@
+<template>
+<h1 class="mt-5">{{place}}</h1>
+</template>
+
+<script>
+export default {
+    name: 'WeatherPrediction',
+    data() {
+        return {
+            place: this.$route.params.place
+        }
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
