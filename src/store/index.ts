00336d329c85ad514c3e569243a3c989f603ab23
Nikola Shumarev <nick_95@mail.bg>
2019-11-25 17:44:44 +0200

    load savedPlaces module

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,14 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import savedPlaces from '@/store/modules/savedPlaces';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+    // state: {},
+    // mutations: {},
+    // actions: {},
+    modules: {
+        savedPlaces
+    }
 });
